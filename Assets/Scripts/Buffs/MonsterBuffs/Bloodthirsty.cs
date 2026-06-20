d8b1503765d70c466a715ae2d1478b481e10aee4
MAFUPC\mafu <matthewfuller3d@gmail.com>
2026-06-19 20:49:23 -0700

    buff to bloodthirsty

--- Assets/Scripts/Buffs/MonsterBuffs/Bloodthirsty.cs
+++ Assets/Scripts/Buffs/MonsterBuffs/Bloodthirsty.cs
@@ -7,9 +7,10 @@ using UnityEngine;
 [CreateAssetMenu(fileName="Bloodthirsty", menuName="Buffs/Monster/Bloodthirsty")]
 public class Bloodthirsty : Buff
 {
+    [SerializeField] int amount = 1;
     public override void OnOtherDie(MonsterCardModel other)
     {
-        Owner.RegisterValueModifier(1);
+        Owner.RegisterValueModifier(amount);
     }
 }
 
